18d51932b2889f4d91d51c27fbb839109f6dca05
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2025-01-16 00:15:46 -0700

    Add files via upload

---
+++ tetris_demo.py
@@ -0,0 +1,544 @@
+def on_button_pressed_a():
+    basic.show_leds("""
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # # .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . # .
+        . . # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # . .
+        . . # . #
+        . # # . #
+        . . . . #
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        . # . . #
+        . # # # #
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        . . . . #
+        . # . . #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # # . . .
+        . . . . #
+        . . . . #
+        . # . . #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . .
+        # # # . #
+        . . . . #
+        . # . . #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        # # . . #
+        # . . . #
+        # # . . #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        # # . . #
+        # # . . #
+        # # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        # # . . #
+        # # . . #
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        # # . . #
+        # # . . #
+        # # # # #
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . #
+        # # . . #
+        # # . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # # .
+        . . . . .
+        . . . . #
+        # # . . #
+        # # . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # # .
+        . . # # .
+        . . . . #
+        # # . . #
+        # # . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . # # .
+        . . # # #
+        # # . . #
+        # # . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # # #
+        # # # # #
+        # # . . #
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . #
+        # # # # #
+        # # # # #
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . #
+        # # # # #
+        # # # # #
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . # #
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . # #
+        . . # # .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . # #
+        . . # # .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # #
+        . . # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . # # . .
+        . . . . .
+        . . . . .
+        . . . # #
+        . . # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # # . . .
+        . # # . .
+        . . . . .
+        . . . # #
+        . . # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # . .
+        . # # . .
+        . # . . .
+        . . . # #
+        . . # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . # . .
+        . # # . .
+        . # . # #
+        . . # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . # #
+        . . . . .
+        . . # . .
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . # #
+        . . # . .
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . . #
+        . . # # #
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . # # . #
+        . . . . #
+        . . # # #
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . # # # #
+        . # . . #
+        . . # # #
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . # # # #
+        . # # # #
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . #
+        . # # # #
+        . # # # #
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . #
+        # # # # #
+        . # # # #
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . #
+        # # # # #
+        # # # # #
+        . # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        # # # # .
+        . . . . .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_string("YOU WIN.")
+input.on_button_pressed(Button.A, on_button_pressed_a)
